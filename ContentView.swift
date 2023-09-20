6e208d210bde6cdab31fed66170034116d802128
Nishan Pathak <npathak5348@stu.d214.org>
2023-09-20 10:39:34 -0500

    star

--- ContentView.swift
+++ ContentView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "star")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("I love golf!!!")
